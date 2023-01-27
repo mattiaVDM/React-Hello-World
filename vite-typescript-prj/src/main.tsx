6159c23191425027751e6e4e98cc4ed915fdfb9a
Mattia van der meer <mattia.vdm@aidia.it>
2023-01-27 09:04:37 +0100

    Set up vite-ts project and wrote basic styling

--- vite-typescript-prj/src/main.tsx
+++ vite-typescript-prj/src/main.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import hearthstoneLogo from "./images/logo.png";
-import "./style.css";
-const root = createRoot(document.getElementById("root"));
+import ReactDOM from "react-dom/client";
+import logo from "./assets/logo.png";
+import "./App.css";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function Header() {
   return (
     <header>
       <nav className="nav">
-        <img src={hearthstoneLogo} alt="logo" className="nav-logo"></img>
+        <img src={logo} className="nav-logo"></img>
+        <h3 className="nav-title">HearthstoneBuilder</h3>
         <ul className="nav-items">
           <li>- Pricing</li>
           <li>- About</li>
@@ -22,11 +23,11 @@ function Header() {
 function MainContent() {
   return (
     <div>
-      <h1> Reasons why i will learn React</h1>
+      <h1 className="page-title"> Reasons why i will learn React</h1>
       <ul>
-        <li>- I want to learn React to get better at functional programming</li>
-        <li>- I want to learn React to build projects with it</li>
-        <li>- I want to learn React to be more hirable</li>
+        <li>I want to learn React to get better at functional programming</li>
+        <li>I want to learn React to build projects with it</li>
+        <li>I want to learn React to be more hirable</li>
       </ul>
     </div>
   );
